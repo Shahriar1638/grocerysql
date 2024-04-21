252ebeb121162e52ab28edc0a4b8e914fdb301da
Shahriar1638 <shafim1638@gmail.com>
2024-04-21 12:31:08 +0600

    payment system added

--- src/handlePayment.php
+++ src/handlePayment.php
@@ -1,8 +1,9 @@
-<!-- Rivan  -->
 <?php
 require_once('DBconnect.php');
 if (isset($_POST['totalcost']) && isset($_POST['customeremail'])){
     $totalcost = $_POST['totalcost'];
+
+    //  cal culating points for customer
     $conversionRate = 10; // 1 point per 10$ shopping
     $points = floor($totalcost / $conversionRate);
     $customeremail = $_POST['customeremail'];
@@ -11,6 +12,8 @@ if (isset($_POST['totalcost']) && isset($_POST['customeremail'])){
     if ($result) {
         while ($row = mysqli_fetch_assoc($result)) {
             $productId = $row['productId'];
+
+            // claculating revenue for sellers 
             $selleremail = $row['selleremail'];
             $productamount = $row['productamount'];
             $price = $row['price'];
@@ -20,19 +23,28 @@ if (isset($_POST['totalcost']) && isset($_POST['customeremail'])){
             if (!$sellerRevenueResult) {
                 echo "Error updating seller revenue for seller with email: $selleremail. Error: " . mysqli_error($conn);
             }
-            // $productId = $row['productId'];
-            // $updateCartCount = "UPDATE products SET cartcount = cartcount + 1 WHERE productId = '$productId'";
-            // $cartCountResult = mysqli_query($conn, $updatecartcount);
-            // if (!$cartCountResult) {
-            //     echo "Error updating sell count for product with ID: $productId. Error: " . mysqli_error($conn);
-            // }
+
+
+            // increamenting sell count for products
+            $productId = $row['productId'];
+            $updateCartCount = "UPDATE products SET cartcount = cartcount + 1 WHERE productId = '$productId'";
+            $cartCountResult = mysqli_query($conn, $updateCartCount);
+            if (!$cartCountResult) {
+                echo "Error updating sell count for product with ID: $productId. Error: " . mysqli_error($conn);
+            }
         }
     } else {
         echo "Error fetching product IDs from cart. Error: " . mysqli_error($conn);
     }
+
+
+    // empting my cart
     $query = "DELETE FROM carts WHERE customeremail = '$customeremail'";
     $result = mysqli_query($conn, $query);
     if ($result){
+
+
+        // adding points for customer
         $query = "UPDATE customers SET points = points + $points WHERE email = '$customeremail'";
         $result = mysqli_query($conn, $query);
         if ($result){
